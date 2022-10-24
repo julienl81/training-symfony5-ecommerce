29cac54fe0d3aa5cc59bc187123d4e17bbc476a4
JulienLaurent77 <julienlaurent77@outlook.com>
2022-10-24 17:03:57 +0200

    fix .env security

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -13,43 +13,43 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
-// class RegisterController extends AbstractController
-// {
-//     /**
-//      * @Route("/inscription", name="app_register")
-//      */
-//     public function index(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher): Response
-//     {
-//         $notification = null;
+class RegisterController extends AbstractController
+{
+    /**
+     * @Route("/inscription", name="app_register")
+     */
+    public function index(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $userPasswordHasher): Response
+    {
+        $notification = null;
 
-//         $user = new User();
-//         $form = $this->createForm(RegisterType::class, $user);
-//         $form->handleRequest($request);
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+        $form->handleRequest($request);
         
-//         if($form->isSubmitted() && $form->isValid()) {
-//         $user = $form->getData();
+        if($form->isSubmitted() && $form->isValid()) {
+        $user = $form->getData();
         
-//         $search_email = $entityManager->getRepository(User::class)->findOneByEmail($user->getEmail());
+        $search_email = $entityManager->getRepository(User::class)->findOneByEmail($user->getEmail());
         
-//             if (!$search_email) {
-//                 $hashedPassword = $userPasswordHasher->hashPassword($user, $user->getPassword());
-//                 $user->setPassword($hashedPassword);
+            if (!$search_email) {
+                $hashedPassword = $userPasswordHasher->hashPassword($user, $user->getPassword());
+                $user->setPassword($hashedPassword);
                 
-//                 //dd($hashedPassword);
-//                 $entityManager->persist($user);
-//                 $entityManager->flush();
-//                 $mail = new Mail();
-//                 $content = "Bonjour ".$user->getFirstname()."<br/>Bienvenue sur la première boutique dédiée au made in France.<br/><br/>";
-//                 $mail->send($user->getEmail(), $user->getFirstname(), 'Bienvenue sur la boutique La Boutique Française', $content);
-//                 $notification = "Votre inscription s'est correctement déroulée. Vous pouvez dès à présent vous connecter à votre compte.";
-//             } else {
-//                 $notification = "L'email que vous avez renseigné existe déjà";
-//             }
-//         }
+                //dd($hashedPassword);
+                $entityManager->persist($user);
+                $entityManager->flush();
+                $mail = new Mail();
+                $content = "Bonjour ".$user->getFirstname()."<br/>Bienvenue sur la première boutique dédiée au made in France.<br/><br/>";
+                $mail->send($user->getEmail(), $user->getFirstname(), 'Bienvenue sur la boutique La Boutique Française', $content);
+                $notification = "Votre inscription s'est correctement déroulée. Vous pouvez dès à présent vous connecter à votre compte.";
+            } else {
+                $notification = "L'email que vous avez renseigné existe déjà";
+            }
+        }
               
-//         return $this->render('register/index.html.twig', [
-//             'form' => $form->createView(),
-//             'notification' => $notification
-//         ]);
-//     }
-// }
+        return $this->render('register/index.html.twig', [
+            'form' => $form->createView(),
+            'notification' => $notification
+        ]);
+    }
+}
